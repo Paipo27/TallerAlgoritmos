3560d3c4b3739d62091cc1afc7a4fe8316c8b242
Daniel <dcarrenoc@unal.edu.co>
2023-11-20 16:45:34 -0500

    git commit -m "Mensaje descriptivo de tus cambios"

---
+++ MenuTallerAl.py
@@ -0,0 +1,68 @@
+#Comnado para importar las funciones del archivo TallerAlgo1.py
+from TallerAlgo1 import *
+
+#Realizado por Daniel Felipe Carreño
+
+#Menu para el usuario
+def menu():
+    # Crear un diccionario vacío para almacenar los arreglos
+    listaarreglos = {}
+    # Ciclo infinito para mostrar el menú
+    while True:
+        print("\n--- Menú ---")
+        print("1. Ingresar Canales")
+        print("2. Mostrar Canales")
+        print("3. Buscar numeros binarios en cada canal")
+        print("4. Cargar Canales desde archivo de texto")
+        print("5. Cargar Canales desde archivo excel")
+        print("6. Estado CanalFuturo")
+        print("7. Matriz EstadoFuturo")
+        print("8. Matriz EstadoCanalIP")
+        print("9. Matriz EstadoEstadoP")
+        print("10. Salir")
+        # Solicitar al usuario la opción
+        opcion = input("Seleccione una opción: ")
+        
+        if opcion == "1":
+            listaarreglos = ingresarCanales()
+        elif opcion == "2":
+            if not listaarreglos:
+                print("Primero debe ingresar los Canales.")
+            else:
+                MostrarCanales(listaarreglos)
+        elif  opcion == "3":
+          ContarBinarios(listaarreglos)
+        elif opcion == "4":
+            listaarreglos = CargarArchivo()
+        elif opcion == "5":
+            file_path = input("Ingrese nombre del archivo Excel: ")
+            try:
+                listaarreglos = CargarExcel(file_path, listaarreglos)
+                print("Canales cargados con éxito desde el archivo Excel.")
+            except Exception as e:
+                print(f"Error al cargar el archivo: {e}")
+        elif opcion == "6":
+             if listaarreglos:
+                generarMatriz(*listaarreglos.values())
+             else:
+               print("No tienes canales ingresados.")
+        elif opcion == "7":
+           if listaarreglos:
+              generarEstados(listaarreglos)
+           else:
+                print("No tienes canales ingresados.")
+        elif opcion == "8":
+            if listaarreglos:
+                generarMatriz(*listaarreglos.values())
+            else:
+                print("No tienes canales ingresados.")
+        elif opcion == "9":
+            if listaarreglos:
+                generarEstadosEstados(listaarreglos)
+            else:
+                print("No tienes canales ingresados.")
+        elif opcion == "10":
+           if print("Gracias por usar el programa"):
+                break
+     #  :3    :/   :v   -_-
+menu()
---
+++ TallerAlgo1.py
@@ -0,0 +1,269 @@
+#collections para crear diccionarios
+from collections import defaultdict
+#intertools para generar las combinaciones de los canales
+from itertools import product
+#pandas para leer el archivo excel
+import pandas as pd
+
+#Realizado por Daniel Felipe Carreño 
+
+#Funcion para ingresar los Canales y que sean binarios
+def ingresarCanales():
+    # Crear un diccionario vacío para almacenar los arreglos
+    listaarreglos = {}
+    numeroCanales = int(input("Ingrese cuántos Canales quiere: "))
+    
+    # Solicita al usuario cuántos datos quiere en los Canales (una vez, y se aplica a todos los canales)
+    valor = int(input("Ingrese cuántos datos quiere en los Canales: "))
+
+    # Ciclo para pedir los datos de cada Canal
+    for i in range(1, numeroCanales + 1):
+        # Crea una lista vacía para guardar los datos
+        nuevoarreglo = []
+        # Solicita al usuario los datos y los agrega a la lista
+        for _ in range(valor):
+            binario = None
+            # Ciclo para validar que el usuario ingrese un número binario
+            while binario not in (0, 1):
+                try:
+                    binario = int(input(f"Ingrese el valor binario para el Canal {i}: "))
+                    if binario not in (0, 1):
+                        print("El valor ingresado no es binario.")
+                    else:
+                        nuevoarreglo.append(binario)
+                except ValueError:
+                    print("El valor ingresado no es un número.")
+        # Agrega el arreglo a la lista de arreglos
+        listaarreglos[i] = nuevoarreglo
+        # Muestra el arreglo ingresado
+        print(f"Canal {i}: {nuevoarreglo}")
+    
+    return listaarreglos
+
+
+# Función para mostrar los Canales ingresados
+def MostrarCanales(listaarreglos):
+    print("\nLos Canales ingresados son: ")
+    for numero, arreglo in listaarreglos.items():
+        #le asigna un numero a cada canal
+        print(f"Canal {numero}: {arreglo}")
+
+# Función para contar los números binarios en los arreglos
+def ContarBinarios(listaarreglos):
+    # Solicitar al usuario los números de los arreglos
+    NuevoCanal = input("Ingrese los números de los Canales separados por comas (ejemplo: 1, 2): ").split(",")
+
+    # Convertir la entrada a una lista de enteros
+    try:
+        NuevoCanal = [int(numero.strip()) for numero in NuevoCanal]
+    except ValueError:
+        print("Ingresó un valor no numérico. Por favor, intente de nuevo.")
+        return
+
+    # Verificar la existencia de los arreglos
+    CanalNoEncontrado = [numero for numero in NuevoCanal if numero not in listaarreglos]
+    if CanalNoEncontrado:
+        print(f"No se encontraron los siguientes Canales: {', '.join(map(str, CanalNoEncontrado))}")
+        return
+
+    # Si todos los arreglos existen, pedir el número binario a buscar
+    binarioAcontar = int(input("Ingrese el valor binario que quiere contar (1 o 0): "))
+    while binarioAcontar not in [0, 1]:
+        print("Valor inválido. Intente de nuevo.")
+        binarioAcontar = int(input("Ingrese el valor binario que quiere contar (1 o 0): "))
+
+    # Contar y mostrar los resultados
+    for numero in NuevoCanal:
+        conteo = listaarreglos[numero].count(binarioAcontar)
+        print(f"El Canal {numero} tiene {conteo} datos del número {binarioAcontar}.")
+
+def CargarArchivo():
+    listaarreglos = {}
+
+    # Pedir el nombre del archivo al usuario
+    NombreArchivo = input("Ingrese el nombre del archivo desde donde cargar los Canales: ")
+    # Abrir el archivo en modo lectura
+    try:
+        with open(NombreArchivo, 'r') as archivo:
+            lineas = archivo.readlines()
+            # Iterar sobre las líneas del archivo
+            for linea in lineas:
+                datos = [int(dato.strip()) for dato in linea.split(",")]
+
+                # Validar si todos los elementos son binarios
+                if not all(d in [0, 1] for d in datos):
+                    print(f"La línea '{linea.strip()}' tiene datos no binarios. Porfavor correguir.")
+                    return {}  # Retornamos un diccionario vacío en caso de error
+
+                listaarreglos[len(listaarreglos) + 1] = datos
+
+            print(f"Se han cargado {len(lineas)} Canales desde el archivo {NombreArchivo}")
+    # Manejo de excepciones error de archivo no encontrado
+    except FileNotFoundError:
+        print(f"No se pudo encontrar el archivo {NombreArchivo}.")
+    # Manejo de excepciones error de datos no numéricos
+    except ValueError:
+        print("El archivo contiene datos no válidos o no numéricos.")
+
+    return listaarreglos
+
+def generarMatriz(*canales):
+    # Definir todos los posibles estados que pueden tener los canales.
+    estados = [''.join(map(str, comb)) for comb in product([0, 1], repeat=len(canales))]
+    
+    # Inicializar el diccionario de transiciones.
+    transiciones = {estado: [0] + [0]*len(canales) for estado in estados}
+    
+    # Recorrer los canales para llenar las transiciones.
+    for i in range(len(canales[0]) - 1):
+        estado_actual = ''.join([str(canal[i]) for canal in canales])
+        transiciones[estado_actual][0] += 1
+        for j, canal in enumerate(canales, start=1):
+            transiciones[estado_actual][j] += canal[i+1]
+    
+    # Mostrar la matriz de transiciones.
+    headers = ' '.join([f"{i:<5}" for i in range(1, len(canales) + 1)])
+    print(f"{' '*5}{headers}")
+    for estado in estados:
+        conteo_total = transiciones[estado][0]
+        if conteo_total == 0:
+            probabilidades = ['{:<5}'.format('0.0') for _ in canales]
+        else:
+            probabilidades = [f"{transiciones[estado][j]/conteo_total:<5.2f}" for j in range(1, len(canales) + 1)]
+        print(f"{estado} {' '.join(probabilidades)}")
+
+def generarEstados(listaarreglos):
+    # Convertir el diccionario a una lista de canales
+    canales = list(listaarreglos.values())
+    
+    # Asegurarse de que al menos hay canales ingresados
+    if not canales:
+        print("No se han ingresado canales. Por favor, añada algunos canales primero.")
+        return
+
+    # Generar matriz
+    estados = [''.join(map(str, comb)) for comb in product([0, 1], repeat=len(canales))]
+    matriz = [[0.0 for _ in range(len(estados))] for _ in range(len(estados))]
+    
+    for idx, estado in enumerate(estados):
+        count = sum([1 for i in range(len(canales[0]) - 1) if 
+                     all([canal[i] == int(bit) for canal, bit in zip(canales, estado)])])
+        if count == 0:
+            continue
+        for j, next_estado in enumerate(estados):
+            occurrences = sum([1 for i in range(len(canales[0]) - 1) if 
+                               all([canal[i] == int(estado[k]) for k, canal in enumerate(canales)]) and 
+                               all([canal[i+1] == int(next_estado[k]) for k, canal in enumerate(canales)])])
+            matriz[idx][j] = round(float(occurrences) / count, 2)
+
+    # Mostrar matriz
+    encabezado = " " * len(estados[0]) + " "
+    for estado in estados:
+        encabezado += "{:^7}".format(estado)
+    print(encabezado)
+    for i, estado_row in enumerate(estados):
+        fila = estado_row + " "
+        for j, estado_col in enumerate(estados):
+            valor = matriz[i][j]
+            fila += "{:^7.2f}".format(valor)
+        print(fila)
+
+def generrEstadoCanalIP(*canales):
+    # Definir todos los posibles estados que pueden tener los canales.
+    estados = [''.join(map(str, comb)) for comb in product([0, 1], repeat=len(canales))]
+    
+    # Inicializar el diccionario de transiciones.
+    transiciones = {estado: [0] + [0]*len(canales) for estado in estados}
+    
+    # Recorrer los canales para llenar las transiciones.
+    for i in range(1, len(canales[0])):
+        estado_actual = ''.join(map(str, [canal[i] for canal in canales]))
+        
+        # Líneas de diagnóstico
+        if estado_actual not in transiciones:
+            print(f"Error: '{estado_actual}' no está en las claves de 'transiciones'.")
+            print(f"Canales: {canales}")
+            return
+        # Fin de líneas de diagnóstico
+        transiciones[estado_actual][0] += 1
+        for j, canal in enumerate(canales, start=1):
+            transiciones[estado_actual][j] += canal[i-1]
+    
+    # Mostrar la matriz de transiciones.
+    headers = ' '.join([f"{i:<5}" for i in range(1, len(canales) + 1)])
+    print(f"{' '*5}{headers}")
+    #For para recorrer los estados y mostrar las probabilidades
+    for estado in estados:
+        conteo_total = transiciones[estado][0]
+        #If para validar si el conteo total es 0
+        if conteo_total == 0:
+            probabilidades = ['{:<5}'.format('0.0') for _ in canales]
+        else:
+            probabilidades = [f"{transiciones[estado][j]/conteo_total:<5.2f}" for j in range(1, len(canales) + 1)]
+        print(f"{estado} {' '.join(probabilidades)}")
+
+from itertools import product
+
+def generarEstadosEstados(listaarreglos):
+    # Convertir el diccionario a una lista de canales
+    canales = list(listaarreglos.values())
+    
+    # Asegurarse de que al menos hay canales ingresados
+    if not canales:
+        print("No se han ingresado canales. Por favor, añada algunos canales primero.")
+        return
+
+    # Generar matriz
+    estados = [''.join(map(str, comb)) for comb in product([0, 1], repeat=len(canales))]
+    matriz = [[0.0 for _ in range(len(estados))] for _ in range(len(estados))]
+    
+    for idx, estado in enumerate(estados):
+        count = sum([1 for i in range(1, len(canales[0])) if  # Iniciamos desde 1, porque miramos atrás
+                     all([canal[i] == int(bit) for canal, bit in zip(canales, estado)])])
+        if count == 0:
+            continue
+        for j, prev_estado in enumerate(estados):  # Ahora estamos buscando el estado anterior, así que lo llamamos prev_estado
+            occurrences = sum([1 for i in range(1, len(canales[0])) if  # Iniciamos desde 1, porque miramos atrás
+                               all([canal[i] == int(estado[k]) for k, canal in enumerate(canales)]) and 
+                               all([canal[i-1] == int(prev_estado[k]) for k, canal in enumerate(canales)])])
+            matriz[idx][j] = round(float(occurrences) / count, 2)
+
+    # Mostrar matriz
+    encabezado = " " * len(estados[0]) + " "
+    #For para recorrer los estados y mostrar las probabilidades
+    for estado in estados:
+        encabezado += "{:^7}".format(estado)
+    print(encabezado)
+    for i, estado_row in enumerate(estados):
+        fila = estado_row + " "
+        for j, estado_col in enumerate(estados):
+            valor = matriz[i][j]
+            fila += "{:^7.2f}".format(valor)
+        print(fila)
+
+
+#Funcion para leer el archivo excel y cargar los canales en el diccionario
+def CargarExcel(file_path, listaarreglos):
+    # Leer el archivo
+    df = pd.read_excel(file_path)
+
+    # Función para procesar el nombre de una columna
+    def process_column_name(column_name):
+        if '.' in str(column_name):
+            parts = str(column_name).split('.')
+            return parts[0]
+        return column_name
+
+    # Procesar los nombres de las columnas
+    df.columns = [process_column_name(col) for col in df.columns]
+
+    # Convertir el excel en una matriz
+    matrix_representation = df.values.tolist()
+
+    # Asignar cada columna de la matriz como un canal en el diccionario
+    for column in matrix_representation:
+        canal_numero = len(listaarreglos) + 1  # El número del canal es el siguiente número disponible
+        listaarreglos[canal_numero] = column
+
+    return listaarreglos  # Devolver el diccionario actualizado
+#  :3    :/   :v   -_-
