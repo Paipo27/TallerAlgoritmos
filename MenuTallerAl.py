3517e97dc191b5539ea97c02f79f03734f2821d2
Daniel <dcarrenoc@unal.edu.co>
2023-11-20 16:56:15 -0500

    git commit -m "Prueba2 de commit"

--- MenuTallerAl.py
+++ MenuTallerAl.py
@@ -1,7 +1,7 @@
 #Comnado para importar las funciones del archivo TallerAlgo1.py
 from TallerAlgo1 import *
 
-#Realizado por Daniel Felipe Carreño y Sofia Leyton 
+#Realizado por Daniel Felipe Carreño 
 
 #Menu para el usuario
 def menu():
